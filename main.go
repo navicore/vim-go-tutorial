2a0914b3c0f00ec42ec9ca56eba30419a42ef36b
Ed Sweeney <ed@onextent.com>
2018-02-03 06:55:46 -0800

    init

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(1)
+	fmt.Println(2)
+	fmt.Println(3)
+	fmt.Println(4)
+	fmt.Println(5)
 }
---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+// func TestFoo(t *testing.T) {
+// 	result := Foo()
+// 	if result != "foo" {
+// 		t.Errorf("expecting foo, got %s", result)
+// 	}
+// }
+//
+// func TestBar(t *testing.T) {
+// 	result := Bar()
+// 	if result != "bar" {
+// 		t.Errorf("expecting bar, got %s", result)
+// 	}
+// }
